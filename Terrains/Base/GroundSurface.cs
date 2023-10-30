b879e44d15c1d03d78b8db0dbc1c56f0170790a5
stephen marshall <stephenb.marshall@yahoo.com>
2023-10-29 17:36:04 -0700

    Added OneBatch and Terrains

---
+++ Terrains/Base/GroundSurface.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GroundSurface {
+
+	public string textureName;
+	public GROUND_SURFACE groundType;
+}
